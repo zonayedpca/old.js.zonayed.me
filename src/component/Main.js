f1ad9f707ce1593b121be1dec8e69d1662802ecc
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-09-02 08:55:45 +0530

    bangla font added

--- src/component/Main.js
+++ src/component/Main.js
@@ -27,13 +27,13 @@ export default class Main extends Component {
 
     return (
       <section className="content-area">
-        {basics.length > 0 ? <Content title="JavaScript Basics" data={basics} color="#F0DB4F" topic="js-basic" /> : <div className="loading-bar"><BarLoader color={'#F0DB4F'} />{delayed ? delayMsg : ''}</div>}
-        {advance.length > 0 ? <Content title="JavaScript Advance" data={advance} color="#F44336" topic="js-advance" /> : <div className="loading-bar"><BarLoader color={'#F44336'} />{delayed ? delayMsg : ''}</div>}
-        {es6.length > 0 ? <Content title="JavaScript ES6" data={es6} color="#BDBDBD" topic="js-es6" /> : <div className="loading-bar"><BarLoader color={'#BDBDBD'} />{delayed ? delayMsg : ''}</div>}
-        {/**dom.length > 0 ? <Content title="JavaScript DOM" data={dom} color="#FFC107" topic="js-dom" /> : <div className="loading-bar"><BarLoader color={'#FFC107'} />{delayed ? delayMsg : ''}</div>**/}
+        {basics.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ব্যাসিক" data={basics} color="#F0DB4F" topic="basic" /> : <div className="loading-bar"><BarLoader color={'#F0DB4F'} />{delayed ? delayMsg : ''}</div>}
+        {advance.length > 0 ? <Content title="জাভাস্ক্রিপ্ট অ্যাডভান্স" data={advance} color="#F44336" topic="advance" /> : <div className="loading-bar"><BarLoader color={'#F44336'} />{delayed ? delayMsg : ''}</div>}
+        {es6.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ইএস৬" data={es6} color="#BDBDBD" topic="es6" /> : <div className="loading-bar"><BarLoader color={'#BDBDBD'} />{delayed ? delayMsg : ''}</div>}
+        {/**dom.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ডম" data={dom} color="#FFC107" topic="js-dom" /> : <div className="loading-bar"><BarLoader color={'#FFC107'} />{delayed ? delayMsg : ''}</div>**/}
         <p style={{textAlign: 'center', marginBottom: '40px', padding: '25px', textTransform: 'uppercase'}}>Posts on JavaScript DOM is Coming Soon, <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/photo.php?fbid=1928931623852850&set=a.108061722606525&type=3&permPage=1">Help me</a> to make the list better</p>
-        {daily.length > 0 ? <Content title="Daily JavaScript" data={daily} color="#4CAF50" topic="js-daily" /> : <div className="loading-bar"><BarLoader color={'#4CAF50'} />{delayed ? delayMsg : ''}</div>}
-        {soft.length > 0 ? <Content title="JavaScript Soft Skills" data={soft} color="#000" topic="js-soft" /> : <div className="loading-bar"><BarLoader color={'#000'} />{delayed ? delayMsg : ''}</div>}
+        {daily.length > 0 ? <Content title="নিত্যদিনের জাভাস্ক্রিপ্ট" data={daily} color="#4CAF50" topic="daily" /> : <div className="loading-bar"><BarLoader color={'#4CAF50'} />{delayed ? delayMsg : ''}</div>}
+        {soft.length > 0 ? <Content title="জাভাস্ক্রিপ্ট সফট স্কিল" data={soft} color="#000" topic="soft" /> : <div className="loading-bar"><BarLoader color={'#000'} />{delayed ? delayMsg : ''}</div>}
       </section>
     )
   }
