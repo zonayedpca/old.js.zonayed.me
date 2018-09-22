345f05b78e25a0d51adda53d6575ac18fe929ff2
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-09-22 10:36:15 +0530

    Mobile Style Updated

--- src/component/Main.js
+++ src/component/Main.js
@@ -24,7 +24,7 @@ export default class Main extends Component {
   }
 
   render() {
-    const {data: {basics, advance, es6, daily, soft}} = this.props;
+    const {data: {basics, advance, es6, dom, daily, algods, soft}} = this.props;
     const { delayed, lastRead, showLastRead } = this.state;
     const delayMsg = <span>I have got a bad server... If your internet connection is not slow, please reload</span>;
 
@@ -37,13 +37,13 @@ export default class Main extends Component {
           <span onClick={() => this.setState({showLastRead:false})}>বন্ধ করুন</span>
           </div>
         </section>}
-        {basics.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ব্যাসিক" data={basics} color="#F0DB4F" topic="basic" /> : <div className="loading-bar"><BarLoader color={'#F0DB4F'} />{delayed ? delayMsg : ''}</div>}
-        {advance.length > 0 ? <Content title="জাভাস্ক্রিপ্ট অ্যাডভান্স" data={advance} color="#F44336" topic="advance" /> : <div className="loading-bar"><BarLoader color={'#F44336'} />{delayed ? delayMsg : ''}</div>}
-        {es6.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ইএস৬" data={es6} color="#BDBDBD" topic="es6" /> : <div className="loading-bar"><BarLoader color={'#BDBDBD'} />{delayed ? delayMsg : ''}</div>}
-        {/**dom.length > 0 ? <Content title="জাভাস্ক্রিপ্ট ডম" data={dom} color="#FFC107" topic="js-dom" /> : <div className="loading-bar"><BarLoader color={'#FFC107'} />{delayed ? delayMsg : ''}</div>**/}
-        <p style={{textAlign: 'center', marginBottom: '40px', padding: '25px', textTransform: 'uppercase'}}>Posts on JavaScript DOM is Coming Soon, <a rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/photo.php?fbid=1928931623852850&set=a.108061722606525&type=3&permPage=1">Help me</a> to make the list better</p>
-        {daily.length > 0 ? <Content title="নিত্যদিনের জাভাস্ক্রিপ্ট" data={daily} color="#4CAF50" topic="daily" /> : <div className="loading-bar"><BarLoader color={'#4CAF50'} />{delayed ? delayMsg : ''}</div>}
-        {soft.length > 0 ? <Content title="জাভাস্ক্রিপ্ট সফট স্কিল" data={soft} color="#000" topic="soft" /> : <div className="loading-bar"><BarLoader color={'#000'} />{delayed ? delayMsg : ''}</div>}
+        {basics.length > 0 ? <Content title="ব্যাসিক" data={basics} color="#F0DB4F" topic="basic" /> : <div className="loading-bar"><BarLoader color={'#F0DB4F'} />{delayed ? delayMsg : ''}</div>}
+        {advance.length > 0 ? <Content title="অ্যাডভান্স" data={advance} color="#F44336" topic="advance" /> : <div className="loading-bar"><BarLoader color={'#F44336'} />{delayed ? delayMsg : ''}</div>}
+        {es6.length > 0 ? <Content title="ইএস৬" data={es6} color="#BDBDBD" topic="es6" /> : <div className="loading-bar"><BarLoader color={'#BDBDBD'} />{delayed ? delayMsg : ''}</div>}
+        {dom.length > 0 ? <Content title="ডম ম্যানিপুলেশন" data={dom} color="#FFC107" topic="dom" /> : <div className="loading-bar"><BarLoader color={'#FFC107'} />{delayed ? delayMsg : ''}</div>}
+        {daily.length > 0 ? <Content title="নিত্যদিনের ব্যবহার" data={daily} color="#4CAF50" topic="daily" /> : <div className="loading-bar"><BarLoader color={'#4CAF50'} />{delayed ? delayMsg : ''}</div>}
+        {algods.length > 0 ? <Content title="অ্যালগরিদম ও ডাটা স্ট্রাকচার" data={algods} color="#315950" topic="algods" /> : <div className="loading-bar"><BarLoader color={'#315950'} />{delayed ? delayMsg : ''}</div>}
+        {soft.length > 0 ? <Content title="সফট স্কিল" data={soft} color="#000" topic="soft" /> : <div className="loading-bar"><BarLoader color={'#000'} />{delayed ? delayMsg : ''}</div>}
       </section>
     )
   }
